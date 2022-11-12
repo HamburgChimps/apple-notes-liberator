1d9e59f01b3b84a4c4238c669c5fb9bd9f01c54b
Alex Olson <alexkellenolson@gmail.com>
2022-11-12 23:09:08 +0100

    add comments explaining the resolver plumbing disable quarkus database devservices as they dont work with the sqlite driver and their failed initialization causes a warning during app startup disable schema generation as it is also not supported for our sqlite driver

--- src/main/java/de/hamburgchimps/NotesDataStoreResolver.java
+++ src/main/java/de/hamburgchimps/NotesDataStoreResolver.java
@@ -13,28 +13,37 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
+// Use the quarkus TenantConnectionResolver interface to determine the jdbc
+// datasource at runtime as opposed to at compile-time. Quarkus still needs a
+// default datasource defined in order to build hence the datasource.* properties in
+// application.properties. Perhaps there is a better way of doing this but this is what
+// I came up with.
+// It feels like a lot of work just to dynamically/programmatically define a datasource.
+// Using props for datasource initialization as demonstrated here ->
+// https://github.com/quarkusio/quarkus/issues/7019#issuecomment-645548780
 @PersistenceUnitExtension
 @ApplicationScoped
-// TODO: @next add comment about why this is so complicated
 public class NotesDataStoreResolver implements TenantConnectionResolver {
     @Override
     public ConnectionProvider resolve(String tenantId) {
         Log.debugv("okay what {0}", tenantId);
         Map<String,String> props = new HashMap<>();
 
-        props.put(AgroalPropertiesReader.MAX_SIZE,"10");
-        props.put(AgroalPropertiesReader.MIN_SIZE,"10");
-        props.put(AgroalPropertiesReader.INITIAL_SIZE,"10");
-        props.put(AgroalPropertiesReader.MAX_LIFETIME_S,"300");
-        props.put(AgroalPropertiesReader.ACQUISITION_TIMEOUT_S,"30");
+        props.put(AgroalPropertiesReader.MAX_SIZE, "1");
+        props.put(AgroalPropertiesReader.MIN_SIZE,"1");
+        props.put(AgroalPropertiesReader.INITIAL_SIZE,"11");
+        props.put(AgroalPropertiesReader.MAX_LIFETIME_S,"30");
+        props.put(AgroalPropertiesReader.ACQUISITION_TIMEOUT_S,"3");
         props.put(AgroalPropertiesReader.JDBC_URL,"jdbc:sqlite:foo");
-        props.put(AgroalPropertiesReader.PRINCIPAL,"username");
-        props.put(AgroalPropertiesReader.CREDENTIAL,"password");
 
         try {
             return new QuarkusConnectionProvider(AgroalDataSource.from(new AgroalPropertiesReader().readProperties(props).get()));
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private String findNotesStoreLocation() {
+        return "";
     }
 }
--- src/main/java/de/hamburgchimps/NotesTenantResolver.java
+++ src/main/java/de/hamburgchimps/NotesTenantResolver.java
@@ -1,11 +1,17 @@
 package de.hamburgchimps;
 
+import io.quarkus.hibernate.orm.PersistenceUnitExtension;
 import io.quarkus.hibernate.orm.runtime.tenant.TenantResolver;
 
 import javax.enterprise.context.ApplicationScoped;
 
+
+// As far as I can tell, this plumbing becomes necessary if quarkus
+// detects that a class that implements TenantConnectionResolver
+// Also as far as I can tell, quarkus only bothers calling resolveTenantId() if
+// there is an active request context. Otherwise, only getDefaultTenantId() is invoked.
+@PersistenceUnitExtension
 @ApplicationScoped
-// TODO: @next-1 add comment about why this is necessary.
 public class NotesTenantResolver implements TenantResolver {
     @Override
     public String getDefaultTenantId() {
@@ -14,6 +20,6 @@ public class NotesTenantResolver implements TenantResolver {
 
     @Override
     public String resolveTenantId() {
-        return "notes";
+        return getDefaultTenantId();
     }
 }
