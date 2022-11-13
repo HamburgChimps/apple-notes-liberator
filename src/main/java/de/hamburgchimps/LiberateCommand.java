20cb2e32d7e7baa9d65fcb6497784aa19627a78e
Alex Olson <alexkellenolson@gmail.com>
2022-11-14 00:00:25 +0100

    remove programmatic datasource configuration in preparation for start of actual project

--- src/main/java/de/hamburgchimps/LiberateCommand.java
+++ src/main/java/de/hamburgchimps/LiberateCommand.java
@@ -1,17 +1,11 @@
 package de.hamburgchimps;
 
-import io.quarkus.arc.Arc;
 import picocli.CommandLine.Command;
-
-import javax.transaction.Transactional;
 
 @Command
 public class LiberateCommand implements Runnable {
     @Override
-    @Transactional
     public void run() {
-        // Fake request context so that quarkus will call our tenant resolver
-        Arc.container().requestContext().activate();
     }
 
 }
