380c8c40a1792a467bc9b7a2a3ae211b57e0368b
Alex Olson <alexkellenolson@gmail.com>
2022-11-15 01:50:09 +0100

    try copying files - too tired to do anything else tonight

--- src/main/java/de/hamburgchimps/LiberateCommand.java
+++ src/main/java/de/hamburgchimps/LiberateCommand.java
@@ -2,9 +2,18 @@ package de.hamburgchimps;
 
 import picocli.CommandLine.Command;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @Command
 public class LiberateCommand implements Runnable {
     @Override
     public void run() {
+        try {
+            Files.copy(Paths.get("./NoteStore.sqlite"), Paths.get("./NoteStoreCopy.sqlite"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
