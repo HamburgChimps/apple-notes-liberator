1bf5a4a24c665f54489f2fd4fdfc7ae5061dd22d
Alex Olson <alexkellenolson@gmail.com>
2022-11-17 22:15:07 +0100

    copy notes db

--- src/main/java/de/hamburgchimps/LiberateCommand.java
+++ src/main/java/de/hamburgchimps/LiberateCommand.java
@@ -4,17 +4,30 @@ import io.quarkus.logging.Log;
 import picocli.CommandLine.Command;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Command
 @SuppressWarnings("unused")
 public class LiberateCommand implements Runnable {
     @Override
     public void run() {
-        Log.info("Look for NoteStore.sqlite file in user dir");
         var noteStoreDb = new File(Constants.NOTE_STORE_PATH);
+
+
         if (!noteStoreDb.exists()) {
-            throw new RuntimeException("Can't find notes file");
+            throw new RuntimeException(UserMessages.CANT_AUTOMATICALLY_FIND_NOTES_DATABASE);
         }
+
+        try {
+            Files.copy(Path.of(Constants.NOTE_STORE_PATH), Path.of("notes.sqlite"));
+        } catch (IOException e) {
+            Log.error(e);
+            throw new RuntimeException(UserMessages.CANT_COPY_NOTES_DATABASE);
+            // TODO: @next implement exception handler and print string there
+        }
+
         Log.info("If found, copy to app dir");
         Log.info("Try to read from it");
         Log.info("If reading from it works, start parsing Notes");
--- src/main/java/de/hamburgchimps/UserMessages.java
+++ src/main/java/de/hamburgchimps/UserMessages.java
@@ -1,7 +1,14 @@
 package de.hamburgchimps;
 
+import picocli.CommandLine.Help.Ansi;
+
 public class UserMessages {
 
-    public static final String CANT_AUTOMATICALLY_FIND_NOTES_DATABASE = "Can't automatically find notes database, you will have to manually provide the ";
+    public static final String CANT_AUTOMATICALLY_FIND_NOTES_DATABASE = createErrorString("Can't automatically find notes database, you will have to manually provide the file path via the -f (--file) option.");
 
+    public static final String CANT_COPY_NOTES_DATABASE = createErrorString(String.format("Can't copy notes database, do you have read and execute permissions for %s?", Constants.NOTE_STORE_PATH));
+
+    private static String createErrorString(String str) {
+        return Ansi.AUTO.string(String.format("@|bold,red %s |@", str));
+    }
 }
