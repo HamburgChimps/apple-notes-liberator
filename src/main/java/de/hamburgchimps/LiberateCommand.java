27f142bcb976b6b64f0696485acb521482017333
Alex Olson <alexkellenolson@gmail.com>
2022-11-17 01:02:17 +0100

    log some steps - again I am too tired to do anything else tonight

--- src/main/java/de/hamburgchimps/LiberateCommand.java
+++ src/main/java/de/hamburgchimps/LiberateCommand.java
@@ -8,5 +8,9 @@ public class LiberateCommand implements Runnable {
     @Override
     public void run() {
         Log.info("This is a command-line app to get your data out of apple notes and into something more accessible");
+        Log.info("Look for NoteStore.sqlite file in user dir");
+        Log.info("If found, copy to app dir");
+        Log.info("Try to read from it");
+        Log.info("If reading from it works, start parsing Notes");
     }
 }
