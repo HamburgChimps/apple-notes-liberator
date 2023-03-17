83a33acdad47cda1e7d8cc3ee3cf5a498f4235a5
Alex Olson <alexkellenolson@gmail.com>
2023-03-17 20:22:57 +0100

    get table column direction

--- src/main/java/de/hamburgchimps/apple/notes/liberator/Constants.java
+++ src/main/java/de/hamburgchimps/apple/notes/liberator/Constants.java
@@ -2,4 +2,6 @@ package de.hamburgchimps.apple.notes.liberator;
 
 public class Constants {
     public static final String NOTE_STORE_PATH = String.format("%s/Library/Group Containers/group.com.apple.notes/notestore.sqlite", System.getProperty("user.home"));
+
+    public static final String TABLE_DIRECTION_KEY_NAME = "crTableColumnDirection";
 }
--- src/main/java/de/hamburgchimps/apple/notes/liberator/data/Table.java
+++ src/main/java/de/hamburgchimps/apple/notes/liberator/data/Table.java
@@ -1,11 +1,15 @@
 package de.hamburgchimps.apple.notes.liberator.data;
 
 import com.ciofecaforensics.Notestore.MergableDataProto;
+import de.hamburgchimps.apple.notes.liberator.Constants;
 import de.hamburgchimps.apple.notes.liberator.ProtoUtils;
 import de.hamburgchimps.apple.notes.liberator.entity.EmbeddedObject;
 import io.quarkus.logging.Log;
 
 public class Table implements EmbeddedObjectData {
+
+    private String direction;
+
     public Table(EmbeddedObject embeddedObject) {
         var result = ProtoUtils.parseProtoUsingParserFromBytes(MergableDataProto.parser(), embeddedObject.zMergeableData);
 
@@ -29,11 +33,27 @@ public class Table implements EmbeddedObjectData {
                 .getMergeableDataObjectData()
                 .getMergeableDataObjectEntryList();
 
-        data.getMergeableDataObjectEntryList().forEach((emtry) -> {
+        data.getMergeableDataObjectEntryList().forEach((entry) -> {
             // each entry is a table object
             // if there is a custom map
             // and first key is "crTableColumnDirection" + 1 ?
             // then first string value is table direction
+
+            // TODO: clean this up!
+            // TODO: why + 1??
+            if (entry.hasCustomMap() && entry.getCustomMap().getMapEntry(0).getKey() == keys.indexOf(Constants.TABLE_DIRECTION_KEY_NAME) + 1) {
+                this.direction = entry.getCustomMap().getMapEntry(0).getValue().getStringValue();
+            }
         });
+
+        Log.debug(this.direction);
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
     }
 }
