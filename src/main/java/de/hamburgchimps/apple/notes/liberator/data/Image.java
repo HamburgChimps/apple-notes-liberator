6e6e58f565b0d6558efdf89dc22d8b7b360d20a9
Alex Olson <alexkellenolson@gmail.com>
2023-04-02 22:58:43 +0200

    make image type field final for #1

--- src/main/java/de/hamburgchimps/apple/notes/liberator/data/Image.java
+++ src/main/java/de/hamburgchimps/apple/notes/liberator/data/Image.java
@@ -3,7 +3,7 @@ package de.hamburgchimps.apple.notes.liberator.data;
 import de.hamburgchimps.apple.notes.liberator.entity.NotesCloudObject;
 
 public class Image implements EmbeddedObjectData {
-    private EmbeddedObjectDataType type = EmbeddedObjectDataType.IMAGE;
+    private final EmbeddedObjectDataType type = EmbeddedObjectDataType.IMAGE;
 
     public Image(NotesCloudObject notesCloudObject) {
 
