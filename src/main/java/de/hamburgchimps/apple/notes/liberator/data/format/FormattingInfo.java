1d2cccdfd457cfe0d5b695caac19e9ab696cfa5a
Alex Olson <alexkellenolson@gmail.com>
2023-04-10 14:15:04 +0200

    add formattinginfo

---
+++ src/main/java/de/hamburgchimps/apple/notes/liberator/data/format/FormattingInfo.java
@@ -0,0 +1,5 @@
+package de.hamburgchimps.apple.notes.liberator.data.format;
+
+import de.hamburgchimps.apple.notes.liberator.data.Markdownable;
+
+public record FormattingInfo(int notePosition, Markdownable markdownable) { }
