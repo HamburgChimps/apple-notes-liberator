af15223c9c23509f0ac029ef8e8722d88054e6b6
Alex Olson <alexkellenolson@gmail.com>
2023-04-14 23:12:38 +0200

    remove space around bold text in generated markdown

--- src/main/java/de/hamburgchimps/apple/notes/liberator/data/format/BoldText.java
+++ src/main/java/de/hamburgchimps/apple/notes/liberator/data/format/BoldText.java
@@ -5,6 +5,6 @@ import de.hamburgchimps.apple.notes.liberator.data.Markdownable;
 public record BoldText(String text) implements Markdownable {
     @Override
     public String toMarkdown() {
-        return String.format("** %s **", text);
+        return String.format("**%s**", text);
     }
 }
