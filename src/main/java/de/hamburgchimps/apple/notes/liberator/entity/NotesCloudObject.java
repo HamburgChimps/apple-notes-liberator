460a1e9ca67ee42bfbfd36669a03e901074ce593
Alex Kellen Olson <alexkellenolson@gmail.com>
2023-06-13 22:26:18 +0200

    remove dead import

--- src/main/java/de/hamburgchimps/apple/notes/liberator/entity/NotesCloudObject.java
+++ src/main/java/de/hamburgchimps/apple/notes/liberator/entity/NotesCloudObject.java
@@ -4,8 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+
 import java.time.LocalDateTime;
 
 @Entity
